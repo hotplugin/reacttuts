c82332f0d44f51d2ba00bb9466e3d4296ad85f70
hotplugin <hotpluginenator@gmail.com>
2019-02-10 19:35:10 +0700

    adding header navigation to all pages

--- hellorouter/src/App.js
+++ hellorouter/src/App.js
@@ -3,21 +3,23 @@ import { BrowserRouter, Route , Switch} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Contact from './components/Contact';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      
       <BrowserRouter>
+      <div className="App">
+      <Header/>
       <Switch>
         <Route path="/" component={Home} exact/>
         <Route path="/contact" component={Contact} />
         <Route component={error} />
       </Switch>
-      </BrowserRouter>
-      
       </div>
-    );
+      </BrowserRouter>
+     );
   }
 }
 
